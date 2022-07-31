d6e669886f43e5c571628e4586667bee4fce8663
Qiwen Shao <72328186+Sspkq@users.noreply.github.com>
2022-07-31 14:29:10 +0800

    Update lengthattack.py

--- lengthattack.py
+++ lengthattack.py
@@ -1,45 +1,18 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*- 
-"""
-@Time：2019/10/12
-@Author: hhzjj
-@Description：SM3长度扩展攻击
-    1.随机生成一个secret，算出secret的hash值
-    2.根据hash值推出第一次压缩之后各个寄存器里的值
-    3.在secret+padding之后附加一段消息，用上一步寄存器里的值作为IV去加密附加的那段消息，得到hash
-    4.用sm3去加密secret+padding+m'，得到hash
-    5.第3步和第4步得到的hash值应该相等
-"""
+
 from gmssl import sm3, func
 import random
-
 import struct
-
 secret = str(random.random())
 secret_hash = sm3.sm3_hash(func.bytes_to_list(bytes(secret, encoding='utf-8')))
 secret_len = len(secret)
 append_m = "1234567890"   # 附加消息
 pad_str = ""
 pad = []
-
-
-
-
 def generate_guess_hash(old_hash, secret_len, append_m):
-    """
-    SM3长度扩展攻击
-    :param old_hash: secret的hash值
-    :param secret_len: secret的长度
-    :param append_m: 附加的消息
-    :return: hash(secret + padding + append_m)
-    """
     vectors = []
     message = ""
-    # 将old_hash分组，每组8个字节, 并转换为整数
     for r in range(0, len(old_hash), 8):
         vectors.append(int(old_hash[r:r + 8], 16))
-
-    # 伪造消息
     if secret_len > 64:
         for i in range(0, int(secret_len / 64) * 64):
             message += 'p'
@@ -80,10 +53,8 @@ def sm3_re(msg, new_v):
     range_end = 56
     if reserve1 > range_end:
         range_end = range_end + 64
-
     for i in range(reserve1, range_end):
         msg.append(0x00)
-
     bit_length = (len1) * 8
     bit_length_str = [bit_length % 0x100]
     for i in range(7):
@@ -91,18 +62,14 @@ def sm3_re(msg, new_v):
         bit_length_str.append(bit_length % 0x100)
     for i in range(8):
         msg.append(bit_length_str[7-i])
-
     group_count = round(len(msg) / 64) - 1
-
     B = []
     for i in range(0, group_count):
         B.append(msg[(i + 1)*64:(i+2)*64])
-
     V = []
     V.append(new_v)
     for i in range(0, group_count):
         V.append(CY(V[i], B[i],i))
-
     y = V[i+1]
     result = ""
     for i in y:
@@ -126,17 +93,12 @@ def padding(msg):
         global pad_str
         pad_str += str(hex(msg[j]))
     return msg
-
-
 guess_hash = generate_guess_hash(secret_hash, secret_len, append_m)
 new_msg = func.bytes_to_list(bytes(secret, encoding='utf-8'))
 new_msg.extend(pad)
 new_msg.extend(func.bytes_to_list(bytes(append_m, encoding='utf-8')))
 new_msg_str = secret + pad_str + append_m
-
 new_hash = smm3(new_msg)
-
-
 print("隐藏的消息: "+secret)
 print("已知的消息长度:%d" % len(secret))
 print("已知的消息哈希:" + secret_hash)
